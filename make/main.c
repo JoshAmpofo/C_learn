a76311a8a5d8e2c303c7604f7f7bc0e601b5129c
JoshAmpofo <joshampofo92@gmail.com>
2023-03-26 16:58:51 +0000

    Add make dir

---
+++ make/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "misc.h"
+/**
+ * main - begin the function
+ *
+ * Return: Hello World str
+ */
+int main(void)
+{
+	printf("Hello, World.\n");
+	myFunc();
+	fflush(stdout);
+
+	return (0);
+}
---
+++ make/misc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "misc.h"
+/**
+ * myFunc - testing makefiles
+ * Return: nothing
+ */
+void myFunc(void)
+{
+	printf("Body of myFunc function.\n");
+}
